1e19cf02ccbf749cb514b49e9e93e8726100f1ad
Ruth <ruthtemp@gmail.com>
2021-09-28 18:27:24 +0200

    warning alert with condition

--- tascam0705/myscript.js
+++ tascam0705/myscript.js
@@ -12,15 +12,7 @@ class Product {
         this.price = price;
         this.year = year;
         
-
-        document.getElementById("displayProduct").innerHTML += `<div class="card" id="${this.name}-card">
-            <div class="card-body">
-                <strong>Product Name: </strong> ${this.name} 
-                <strong>Product Price: </strong> ${this.price}
-                <strong>Product Year: </strong> ${this.year}  
-                <button type="button" class="btn btn-danger delete" id="${this.name}">Delete</button>
-            </div>
-        </div>`
+        
 
     // SHOW ALERT "SUCCESS"
          document.getElementById("alert").innerHTML += `<div class="alert alert-success" role="alert" id="${this.name}-success">
@@ -33,28 +25,34 @@ class Product {
                 document.getElementById(`${this.name}-success`)
             )
         },5000)
-        
-        
+                
+
+    //delete button eventlistener
 
         document.addEventListener("click", (event)=>{
-            if(event.target.id === this.name){
+            if(event.target.id === `${this.name}-delete`){
                 this.delete();
-             }
+                }
         })
+        
+        
+        
     }
+
+    // delete method
 
     delete(){
         document.getElementById("displayProduct").removeChild(
             document.getElementById(`${this.name}-card`)
         )
-        document.getElementById("alert").innerHTML += `<div class="${this.name} alert alert-danger" role="alert" id="${this.name}-delete">
+        document.getElementById("alert").innerHTML += `<div class="${this.name} alert alert-danger" role="alert" id="${this.name}-deletealert">
         Product Deleted Sucessfully!
       </div>`
 
     // FADE DELETE ALERT
       setTimeout(()=>{
         document.getElementById("alert").removeChild(
-            document.getElementById(`${this.name}-delete`)
+            document.getElementById(`${this.name}-deletealert`)
         )
     },5000)
     
@@ -63,57 +61,36 @@ class Product {
 
 document.getElementById("saveProduct").addEventListener("click", function() {
 
-    checkIfEmpty("productName")
-    checkIfEmpty("productPrice")
-    checkIfEmpty("productYear")
-
-    
-    const productName = document.getElementById("productName").value ;
-    const productPrice = document.getElementById("productPrice").value ;
-    const productYear = document.getElementById("productYear").value ;
-    
     // cocacola
     // 2
     // 1990
-    const newProduct = new Product (productName, productPrice, productYear)
-    // console.log(newProduct)
+    const productName = document.getElementById("productName").value ;
+    const productPrice = document.getElementById("productPrice").value ;
+    const productYear = document.getElementById("productYear").value ;
 
-});
-
-
-function checkIfEmpty(id) {
-
-    let input = document.getElementById(id)
-
-    if (input.value === "") {
-        document.getElementById(id).classList.add("is-invalid")
-        document.getElementById("alert").innerHTML = `<div class="${this.name} alert alert-warning" role="alert" id="${this.name}-warning">
-        Complete Required Fields Please
-        </div>`
-    } else {
-        document.getElementById(id).classList.remove("is-invalid")
-        document.getElementById("alert").removeChild(
-            document.getElementById(`${this.name}-warning`)
-        )
-    }
-
-};
-
-
-
-// falta crear condició: quan cap sigui invalid es crea l'objecte
     
 
+    if(productName  && productPrice  && productYear ){
+        
+        const newProduct = new Product (productName, productPrice, productYear)
 
+        document.getElementById("displayProduct").innerHTML += `<div class="card" id="${productName}-card">
+            <div class="card-body">
+                <strong>Product Name: </strong> ${productName} 
+                <strong>Product Price: </strong> ${productPrice}
+                <strong>Product Year: </strong> ${productYear}  
+                <button type="button" class="btn btn-danger delete" id="${productName}-delete">Delete</button>
+            </div>
+        </div>`
+        document.getElementById("alert-warning").innerHTML = ""
+        
+    }else{
+        document.getElementById("alert-warning").innerHTML = `<div class="${productName} alert alert-warning" role="alert" id="${this.name}-warning">
+                 Complete Required Fields Please
+                </div>`
+    }
+    
+});
 
-// addEventListener("click", function(event) {
-//     if(event.target.classList.contains("delete")){
-//       document.getElementById("displayProduct").removeChild(
-//   document.getElementById(`${event.target.id}-card`)
-//         //  event.target.parentNode.parentNode 
-//       )
-//     }
-
-// });
 
 
